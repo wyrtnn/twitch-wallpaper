4417bc8af8668fe482312c9f71056d88a30c6172
wyrtnn <135952609+wyrtnn@users.noreply.github.com>
2025-03-21 13:22:11 +0200

    main twitch-wallpaper version 1

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const { exec } = require('child_process');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    
+    mainWindow = new BrowserWindow({
+        width: 1920,         
+        height: 1080,        
+        frame: false,        
+        alwaysOnTop: false,  
+        transparent: true,   
+        fullscreen: true,    
+        webPreferences: { nodeIntegration: false }
+    });
+
+    mainWindow.loadURL('https://www.twitch.tv/название_канала');
+
+    mainWindow.setSkipTaskbar(true);
+
+    app.on('ready', () => {
+        exec('powershell -Command "& {[System.Runtime.Interopservices.Marshal]::WriteInt32((Get-Process -id $pid).MainWindowHandle, -20, 0x80)}"');
+    });
+});
